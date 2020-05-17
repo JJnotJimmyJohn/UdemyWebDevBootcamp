aa4e80c423ce1f077f98c29db8cad6ef378756a9
jianjin <jxj362@case.edu>
2020-05-17 18:00:36 -0400

    EJS conditionals and loops

--- EJSdemo/app.js
+++ EJSdemo/app.js
@@ -7,9 +7,19 @@ app.get("/", function(req,res){
 	res.send("you are here");
 });
 
+app.get("/posts",function(req,res){
+	var posts = [
+		{title:"i'm switching from PyCharm to VS Code", author:"JJ"},
+		{title:"today it rained again.", author:"Jian"},
+		{title:"my shoulder hurts a little",author:"JiJi"}
+	];
+	res.render("posts.ejs",{posts:posts});
+})
+
 app.get("/persons/:name",function(req,res){
 //	var name = req.params["name"];
 	res.render("namepage.ejs",{name:req.params.name});
+	console.log("namepage is visited");
 });
 
 app.listen("8008","172.17.0.2",function(){
